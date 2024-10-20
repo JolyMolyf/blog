2282859ccb6f144fc6ed35835dda9cac1433d8bd
JolyMolyF <vs2001dor@gmail.com>
2024-10-20 22:25:11 +0200

    Hover effect for contact me cards

--- src/app/sections/contact/Contact.tsx
+++ src/app/sections/contact/Contact.tsx
@@ -9,6 +9,7 @@ const contactMethods = [
         name: 'Call me',
         value: '+48664207663',
         icon: './icons/phone-solid.svg',
+        hoverIcon: './icons/white/phone-solid-white.svg',
         description: 'Mon-Sat from 8am to 10pm',
         url: '/',
         action: () => {}
@@ -17,6 +18,7 @@ const contactMethods = [
         name: 'Email me',
         value: 'vsevolod.doroszenko@gmail.com',
         icon: './icons/envelope-regular.svg',
+        hoverIcon: './icons/white/envelope-solid-white.svg',
         description: 'Any time',
         url: 'mailto:vsevolod.doroszenko@gmail.com',
         action: () => {}
@@ -25,6 +27,7 @@ const contactMethods = [
         name: 'LinkedIn',
         value: '',
         icon: './icons/linkedin-in-brands-solid.svg',
+        hoverIcon: './icons/white/linkedin-in-brands-solid-white.svg',
         description: 'Any time',
         url: 'https://www.linkedin.com/in/vsevolod-doroshenko/',
         action: () => {}
@@ -33,6 +36,7 @@ const contactMethods = [
         name: 'Instagram',
         value: '',
         icon: './icons/instagram-brands-solid.svg',
+        hoverIcon: './icons/white/instagram-brands-solid-white.svg',
         description: 'Let`s get acquainted',
         url: 'https://www.instagram.com/joly_molyf/',
         action: () => {}
@@ -61,7 +65,8 @@ const Contact = () => {
                     return (
                         <div className='contact-methods-card' key={index} onClick={() => {handleCardClick(method.url)}}>
                             <div className='contact-methods-card-icon'>
-                                <img src={method.icon}/>
+                                <img className='icon-initial' src={method.icon}/>
+                                <img className='icon-hover' src={method.hoverIcon}/>
                             </div>
                             <div className='contact-methods-card-body'>
                                 <p className='highlighted-main-text bold'>{method.name}</p>
